05a4df61b63ae2c942063c1d7a10b9c3153969d4
Tiger287-bit <3539978106@qq.com>
2026-08-23 21:08:42 +0800

    初始化咸鱼哥知识小库

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = '咸鱼哥知识小库';
+export const SITE_DESCRIPTION = '记录机器人、嵌入式与端侧 AI 的学习与实践。';
