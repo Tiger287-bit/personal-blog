64db0cbc37e8b93bfea0f4b16a0fb0ebd6ebebe0
Tiger287-bit <3539978106@qq.com>
2026-08-23 21:41:17 +0800

    配置博客正式域名

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://personal-blog-183.pages.dev',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
